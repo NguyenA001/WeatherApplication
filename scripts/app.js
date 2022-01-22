6480c7a6f29dec60a03666a6e2ec83785b8f9c14
NguyenA001 <annguyen8545@gmail.com>
2022-01-21 16:08:24 -0800

    01/21/2022 EOD

--- scripts/app.js
+++ scripts/app.js
@@ -2,81 +2,249 @@ import {weatherDefault, getDefaultCity, getCityWeather} from './scripts.js';
 import convertoF from './conversions.js';
 
 let currentDegrees = document.getElementById('currentDegrees'),
- currentLowDegrees = document.getElementById('currentLowDegrees'),
- currentHighDegrees = document.getElementById('currentHighDegrees'),
- currenWind = document.getElementById('currenWind'),
- currentHumidity = document.getElementById('currentHumidity'),
- currentDegrees1 = document.getElementById('currentDegrees1'),
- currentLowDegrees1 = document.getElementById('currentLowDegrees1'),
- currentHighDegrees1 = document.getElementById('currentHighDegrees1'),
- currentWind1 = document.getElementById('currentWind1'),
- currentHumidity1 = document.getElementById('currentHumidity1'),
- currentDegrees2 = document.getElementById('currentDegrees2'),
- currentLowDegrees2 = document.getElementById('currentLowDegrees2'),
- currentHighDegrees2 = document.getElementById('currentHighDegrees2'),
- currentWind2 = document.getElementById('currentWind2'),
- currentHumidity2 = document.getElementById('currentHumidity2'),
- currentDegrees3 = document.getElementById('currentDegrees3'),
- currentLowDegrees3 = document.getElementById('currentLowDegrees3'),
- currentHighDegrees3 = document.getElementById('currentHighDegrees3'),
- currentWind3 = document.getElementById('currentWind3'),
- currentHumidity3 = document.getElementById('currentHumidity3'),
- currentDegrees4 = document.getElementById('currentDegrees4'),
- currentLowDegrees4 = document.getElementById('currentLowDegrees4'),
- currentHighDegrees4 = document.getElementById('currentHighDegrees4'),
- currentWind4 = document.getElementById('currentWind4'),
- currentHumidity4 = document.getElementById('currentHumidity4'),
- currentDegrees5 = document.getElementById('currentDegrees5'),
- currentLowDegrees5 = document.getElementById('currentLowDegrees5'),
- currentHighDegrees5 = document.getElementById('currentHighDegrees5'),
- currentWind5 = document.getElementById('currentWind5'),
- currentHumidity5 = document.getElementById('currentHumidity5'),
- cityHead = document.getElementById('cityHead'),
- favoriteBTN = document.getElementById('favoriteBTN');
+    currentLowDegrees = document.getElementById('currentLowDegrees'),
+    currentHighDegrees = document.getElementById('currentHighDegrees'),
+    currenWind = document.getElementById('currenWind'),
+    currentHumidity = document.getElementById('currentHumidity'),
+    currentDegrees1 = document.getElementById('currentDegrees1'),
+    currentLowDegrees1 = document.getElementById('currentLowDegrees1'),
+    currentHighDegrees1 = document.getElementById('currentHighDegrees1'),
+    currentWind1 = document.getElementById('currentWind1'),
+    currentHumidity1 = document.getElementById('currentHumidity1'),
+    currentDegrees2 = document.getElementById('currentDegrees2'),
+    currentLowDegrees2 = document.getElementById('currentLowDegrees2'),
+    currentHighDegrees2 = document.getElementById('currentHighDegrees2'),
+    currentWind2 = document.getElementById('currentWind2'),
+    currentHumidity2 = document.getElementById('currentHumidity2'),
+    currentDegrees3 = document.getElementById('currentDegrees3'),
+    currentLowDegrees3 = document.getElementById('currentLowDegrees3'),
+    currentHighDegrees3 = document.getElementById('currentHighDegrees3'),
+    currentWind3 = document.getElementById('currentWind3'),
+    currentHumidity3 = document.getElementById('currentHumidity3'),
+    currentDegrees4 = document.getElementById('currentDegrees4'),
+    currentLowDegrees4 = document.getElementById('currentLowDegrees4'),
+    currentHighDegrees4 = document.getElementById('currentHighDegrees4'),
+    currentWind4 = document.getElementById('currentWind4'),
+    currentHumidity4 = document.getElementById('currentHumidity4'),
+    currentDegrees5 = document.getElementById('currentDegrees5'),
+    currentLowDegrees5 = document.getElementById('currentLowDegrees5'),
+    currentHighDegrees5 = document.getElementById('currentHighDegrees5'),
+    currentWind5 = document.getElementById('currentWind5'),
+    currentHumidity5 = document.getElementById('currentHumidity5'),
+    currentIcon = document.getElementById('currentIcon'),
+    icon1 = document.getElementById('icon1'),
+    icon2 = document.getElementById('icon2'),
+    icon3 = document.getElementById('icon3'),
+    icon4 = document.getElementById('icon4'),
+    icon5 = document.getElementById('icon5'),
+    cityHead = document.getElementById('cityHead'),
+
+    userInputCity = document.getElementById('userInputCity'),
+    searchCityBTN = document.getElementById('searchCityBTN'),
+
+    day1 = document.getElementById('day1'),
+    day2 = document.getElementById('day2'),
+    day3 = document.getElementById('day3'),
+    day4 = document.getElementById('day4'),
+    day5 = document.getElementById('day5'),
+    currentDay = document.getElementById('currentDay'),
+
+    currentDATE = document.getElementById('currentDATE'),
+    Date1 = document.getElementById('Date1'),
+    Date2 = document.getElementById('Date2'),
+    Date3 = document.getElementById('Date3'),
+    Date4 = document.getElementById('Date4'),
+    Date5 = document.getElementById('Date5'),
+
+    card1 = document.getElementById('card1'),
+    card2 = document.getElementById('card2'),
+    card3 = document.getElementById('card3'),
+    card4 = document.getElementById('card4'),
+    card5 = document.getElementById('card5');
+
 
 
 async function displayDefault(){
     await getDefaultCity();
     console.log(weatherDefault);
+    displayData();
+
+};
+
+displayDefault();
+
+
+searchCityBTN.addEventListener('click', async function(e){
+    await getCityWeather(userInputCity.value);
+    console.log(await getCityWeather(userInputCity.value));
+    displayData();
+
+    let data = GetLocalStorage();
+    if(data.includes(weatherDefault.city.name))
+    {
+        favoriteBTN.src = "./images/RedHeart.png"
+        favON = true;
+    }
+    else
+    {
+        favoriteBTN.src = "./images/Heart.png"
+        favON = false;
+    }
+});
+
+card1.addEventListener('click', function(){
+    currentDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp)}°F`;
+    currentLowDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp_min)}°F`;
+    currentHighDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp_max)}°F`;
+    currenWind.textContent = `${(weatherDefault.list[0].wind.speed).toFixed(1)}km/hr`;
+    currentHumidity.textContent = `${weatherDefault.list[0].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[0].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[0].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[0].dt_txt);
+});
+
+card2.addEventListener('click', function(){
+    currentDegrees.textContent = `${convertoF(weatherDefault.list[8].main.temp)}°F`;
+    currentLowDegrees.textContent = `${convertoF(weatherDefault.list[8].main.temp_min)}°F`;
+    currentHighDegrees.textContent = `${convertoF(weatherDefault.list[8].main.temp_max)}°F`;
+    currenWind.textContent = `${(weatherDefault.list[8].wind.speed).toFixed(1)}km/hr`;
+    currentHumidity.textContent = `${weatherDefault.list[8].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[8].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[8].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[8].dt_txt);
+});
+card3.addEventListener('click', function(){
+    currentDegrees.textContent = `${convertoF(weatherDefault.list[16].main.temp)}°F`;
+    currentLowDegrees.textContent = `${convertoF(weatherDefault.list[16].main.temp_min)}°F`;
+    currentHighDegrees.textContent = `${convertoF(weatherDefault.list[16].main.temp_max)}°F`;
+    currenWind.textContent = `${(weatherDefault.list[16].wind.speed).toFixed(1)}km/hr`;
+    currentHumidity.textContent = `${weatherDefault.list[16].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[16].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[16].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[16].dt_txt);
+});
+card4.addEventListener('click', function(){
+    currentDegrees.textContent = `${convertoF(weatherDefault.list[24].main.temp)}°F`;
+    currentLowDegrees.textContent = `${convertoF(weatherDefault.list[24].main.temp_min)}°F`;
+    currentHighDegrees.textContent = `${convertoF(weatherDefault.list[24].main.temp_max)}°F`;
+    currenWind.textContent = `${(weatherDefault.list[24].wind.speed).toFixed(1)}km/hr`;
+    currentHumidity.textContent = `${weatherDefault.list[24].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[24].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[24].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[24].dt_txt);
+});
+card5.addEventListener('click', function(){
+    currentDegrees.textContent = `${convertoF(weatherDefault.list[32].main.temp)}°F`;
+    currentLowDegrees.textContent = `${convertoF(weatherDefault.list[32].main.temp_min)}°F`;
+    currentHighDegrees.textContent = `${convertoF(weatherDefault.list[32].main.temp_max)}°F`;
+    currenWind.textContent = `${(weatherDefault.list[32].wind.speed).toFixed(1)}km/hr`;
+    currentHumidity.textContent = `${weatherDefault.list[32].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[32].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[32].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[32].dt_txt);
+});
+
+function displayWeekDay(time){
+    let d = new Date(time);
+    let weekday = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+    return weekday[d.getUTCDay()];
+}
+
+function displayDate(x){
+    let DATEdisplay = (x).split(' ');
+    return DATEdisplay[0];
+}
+
+import {RemoveFromLocalStorage, SaveToLocalStoragebyCityName, creatingFavoriteList, emptyHTML, GetLocalStorage} from './favorites.js'
+let favoriteBTN = document.getElementById('favoriteBTN');
+let favON = false;
+
+favoriteBTN.addEventListener('click', function(){
+
+    if(favON == true){
+        favoriteBTN.src = "./images/Heart.png"
+        RemoveFromLocalStorage(weatherDefault.city.name)
+        emptyHTML(weatherDefault.city.name);
+    }
+    else{
+        favoriteBTN.src = "./images/RedHeart.png"
+        SaveToLocalStoragebyCityName(weatherDefault.city.name)
+        creatingFavoriteList(weatherDefault.city.name);
+    }
+    favON = !favON;
+
+});
+
+GetLocalStorage();
+
+// function favoriteCLICK(cityName){
+//     let favoriteCities = document.getElementById(cityName);
+//     favoriteCities.addEventListener('click', function(cityName){
+//         getCityWeather(cityName);
+//         displayData();
+//     });
+// }
+
+// addEventListener('click', function(cityName){
+//     getCityWeather(cityName);
+//     displayData();
+// });
+
+
+function displayData(){
     currentDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp)}°F`;
     currentLowDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp_min)}°F`;
     currentHighDegrees.textContent = `${convertoF(weatherDefault.list[0].main.temp_max)}°F`;
     currenWind.textContent = `${(weatherDefault.list[0].wind.speed).toFixed(1)} km/hr`;
     currentHumidity.textContent = `${weatherDefault.list[0].main.humidity}%`;
+    currentIcon.src = `./images/${weatherDefault.list[0].weather[0].icon}.png`;
+    currentDay.textContent = displayWeekDay(weatherDefault.list[0].dt_txt);
+    currentDATE.textContent = displayDate(weatherDefault.list[0].dt_txt);
 
     currentDegrees1.textContent = `${convertoF(weatherDefault.list[0].main.temp)}°F`;
     currentLowDegrees1.textContent = `${convertoF(weatherDefault.list[0].main.temp_min)}°F`;
     currentHighDegrees1.textContent = `${convertoF(weatherDefault.list[0].main.temp_max)}°F`;
     currentWind1.textContent = `${(weatherDefault.list[0].wind.speed).toFixed(1)}km/hr`;
     currentHumidity1.textContent = `${weatherDefault.list[0].main.humidity}%`;
+    icon1.src = `./images/${weatherDefault.list[0].weather[0].icon}.png`;
+    day1.textContent = displayWeekDay(weatherDefault.list[0].dt_txt);
+    Date1.textContent = displayDate(weatherDefault.list[0].dt_txt);
 
     currentDegrees2.textContent = `${convertoF(weatherDefault.list[8].main.temp)}°F`;
     currentLowDegrees2.textContent = `${convertoF(weatherDefault.list[8].main.temp_min)}°F`;
     currentHighDegrees2.textContent = `${convertoF(weatherDefault.list[8].main.temp_max)}°F`;
     currentWind2.textContent = `${(weatherDefault.list[8].wind.speed).toFixed(1)}km/hr`;
     currentHumidity2.textContent = `${weatherDefault.list[8].main.humidity}%`;
+    icon2.src = `./images/${weatherDefault.list[8].weather[0].icon}.png`;
+    day2.textContent = displayWeekDay(weatherDefault.list[8].dt_txt);
+    Date2.textContent = displayDate(weatherDefault.list[8].dt_txt);
 
     currentDegrees3.textContent = `${convertoF(weatherDefault.list[16].main.temp)}°F`;
     currentLowDegrees3.textContent = `${convertoF(weatherDefault.list[16].main.temp_min)}°F`;
     currentHighDegrees3.textContent = `${convertoF(weatherDefault.list[16].main.temp_max)}°F`;
     currentWind3.textContent = `${(weatherDefault.list[16].wind.speed).toFixed(1)}km/hr`;
     currentHumidity3.textContent = `${weatherDefault.list[16].main.humidity}%`;
+    icon3.src = `./images/${weatherDefault.list[16].weather[0].icon}.png`;
+    day3.textContent = displayWeekDay(weatherDefault.list[16].dt_txt);
+    Date3.textContent = displayDate(weatherDefault.list[16].dt_txt);
 
     currentDegrees4.textContent = `${convertoF(weatherDefault.list[24].main.temp)}°F`;
     currentLowDegrees4.textContent = `${convertoF(weatherDefault.list[24].main.temp_min)}°F`;
     currentHighDegrees4.textContent = `${convertoF(weatherDefault.list[24].main.temp_max)}°F`;
     currentWind4.textContent = `${(weatherDefault.list[24].wind.speed).toFixed(1)}km/hr`;
     currentHumidity4.textContent = `${weatherDefault.list[24].main.humidity}%`;
+    icon4.src = `./images/${weatherDefault.list[24].weather[0].icon}.png`;
+    day4.textContent = displayWeekDay(weatherDefault.list[24].dt_txt);
+    Date4.textContent = displayDate(weatherDefault.list[24].dt_txt);
 
     currentDegrees5.textContent = `${convertoF(weatherDefault.list[32].main.temp)}°F`;
     currentLowDegrees5.textContent = `${convertoF(weatherDefault.list[32].main.temp_min)}°F`;
     currentHighDegrees5.textContent = `${convertoF(weatherDefault.list[32].main.temp_max)}°F`;
     currentWind5.textContent = `${(weatherDefault.list[32].wind.speed).toFixed(1)}km/hr`;
     currentHumidity5.textContent = `${weatherDefault.list[32].main.humidity}%`;
+    icon5.src = `./images/${weatherDefault.list[32].weather[0].icon}.png`;
+    day5.textContent = displayWeekDay(weatherDefault.list[32].dt_txt);
+    Date5.textContent = displayDate(weatherDefault.list[32].dt_txt);
 
     cityHead.textContent = `${weatherDefault.city.name}`
-};
-
-displayDefault();
-
-
+}
--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,11 +1,54 @@
-let favON = false;
+let favorites = [];
 
-favoriteBTN.addEventListener('click', function(){
-    if(favON == true){
-        favoriteBTN.src = "./images/Heart.png"
-    }
-    else{
-        favoriteBTN.src = "./images/RedHeart.png"
-    }
-    favON = !favON;
-});
+function SaveToLocalStoragebyCityName(cityName)
+{
+    favorites.push(cityName);
+    localStorage.setItem('Favorites', JSON.stringify(favorites));
+}
+
+function SaveToLocalStorage()
+{
+    localStorage.setItem('Favorites', JSON.stringify(favorites));
+}
+
+function RemoveFromLocalStorage(cityName){
+    let cityIndex = favorites.indexOf(cityName);
+    favorites.splice(cityIndex,1);
+    SaveToLocalStorage();
+}
+
+function GetLocalStorage(){
+    const localStorageData = localStorage.getItem('Favorites');
+    favorites = JSON.parse(localStorageData);
+    return favorites;
+}
+
+//LOCAL STORAGE-----------------------------------------------------
+
+let injectHere = document.getElementById('injectHere');
+
+function creatingFavoriteList(cityName){
+    var a = document.createElement('a');
+    a.className = "dropdown-item textWhite";
+    a.textContent = cityName;
+    a.id = cityName;
+    let li = document.createElement('li');
+    li.appendChild(a);
+    return injectHere.appendChild(li);
+}
+
+function emptyHTML(cityName){
+    let x = document.getElementById(cityName);
+    x.remove();
+}
+
+// let cityName = document.getElementById('cityName');
+
+// a.id.addEventListener('click', function(cityName){
+//     getCityWeather(cityName);
+//     displayData();
+// });
+
+
+
+export {RemoveFromLocalStorage, SaveToLocalStoragebyCityName, creatingFavoriteList, emptyHTML, GetLocalStorage}
